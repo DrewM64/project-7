5058cd51b039082b301c740caa0b0ad9b79ced1c
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2021-04-27 18:20:34 -0400

    CSS alert banner
    -Alert banner styled
    -nav bar styled
    -Padding overflow issue fixed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const send = document.getElementById("send");
 alertBanner.innerHTML = 
 `
     <div class =  "alert-banner">
-        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete.</p>
+        <p><strong>Alert:</strong> You have unread messages.</p>
         <p class="alert-banner-close">x</p>
     </div>
 `;
@@ -30,6 +30,7 @@ let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
     "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
+        label: 'Traffic',
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
         2500],
         backgroundColor: 'rgba(116, 119, 191, .3)',
